de9e933efc3724bb5ba23a88d0e4530865eb5c75
Jeff Filipovits <jrfilipovits@gmail.com>
2020-03-20 08:30:51 -0400

    moved all parsing to one function

--- elgantt-parse.el
+++ elgantt-parse.el
@@ -5,29 +5,55 @@
 ;;    b. otherwise, insert a new heading
 ;; 4. insert the entry by changing the text properties at point
 
-(defun jrf/elgantt--get-root-heading ()
-  (save-excursion 
-    (while (org-up-heading-safe))
-    (cdar (org-entry-properties (point) "ITEM"))))
-
-(defun gantt--get-inactive-timestamp ()
-  (cdar (org-entry-properties (point) "TIMESTAMP_IA")))
-
-(defun gantt--get-active-timestamp ()
-  (cdar (org-entry-properties (point) "TIMESTAMP")))
-	   
-(defun gantt--get-category ()
-  (cdar (org-entry-properties (point) "CATEGORY")))
-
-(defun gantt--get-hashtag ()
-  (catch 'hashtag
-    (dolist (tag (s-split ":" 
-			  (org-no-properties 
-			   (cdar (org-entry-properties (point) "ALLTAGS")))))
-      (when (s-starts-with-p "#" tag)
-	(throw 'hashtag (substring-no-properties tag))))))
-
-
+(defun elgantt-parse::get (prop)
+  "Accepts the following arguments: `root', `timestamp', 
+`timestamp-ia', `category', `hashtag', `timestamp-range', `timestamp-ia-range',
+`deadline'. PROP may also be a function to get heading information, 
+which is claled with POINT at the first point of the org headline."
+  (pcase prop
+    ((pred functionp) (funcall prop))
+    ('root
+     (save-excursion 
+       (while (org-up-heading-safe))
+       (cdar (org-entry-properties (point) "ITEM"))))
+    ('timestamp
+     (cond ((string= (cdar (org-entry-properties (point) "TIMESTAMP")) (elgantt--get-property-from-org-point)) nil)
+	   ((not (s-match "--" (cdar (org-entry-properties (point) "TIMESTAMP")))) nil)
+	   (t
+	    (let ((dates (s-split "--" (cdar (org-entry-properties (point) "TIMESTAMP")))))
+	      (list (elgantt--normalize-date-string (car dates)) (elgantt--normalize-date-string (cadr dates)))))))
+    ('timestamp-range
+     (when (cdar (org-entry-properties (point) "TIMESTAMP"))
+       (cond ((string= (cdar (org-entry-properties (point) "TIMESTAMP")) (elgantt--get-property-from-org-point)) nil)
+	     ((not (s-match "--" (cdar (org-entry-properties (point) "TIMESTAMP")))) nil)
+	     (t
+	      (let ((dates (s-split "--" (cdar (org-entry-properties (point) "TIMESTAMP_IA")))))
+		(list (elgantt--normalize-date-string (car dates)) (elgantt--normalize-date-string (cadr dates))))))))
+    ('timestamp-ia
+     (when (cdr (car (org-entry-properties (point) "TIMESTAMP_IA")))
+       (cond ((string= (cdr (car (org-entry-properties (point) "TIMESTAMP_IA"))) (elgantt--get-property-from-org-point)) nil)
+	     ((s-match "--" (cdar (org-entry-properties (point) "TIMESTAMP_IA"))) nil)
+	     (t
+	      (elgantt--normalize-date-string (cdr (car (org-entry-properties (point) "TIMESTAMP_IA"))))))))       
+    ('timestamp-ia-range
+     (when (cdar (org-entry-properties (point) "TIMESTAMP_IA"))
+       (cond ((string= (cdar (org-entry-properties (point) "TIMESTAMP_IA")) (elgantt--get-property-from-org-point)) nil)
+	     ((not (s-match "--" (cdar (org-entry-properties (point) "TIMESTAMP_IA")))) nil)
+	     (t
+	      (let ((dates (s-split "--" (cdar (org-entry-properties (point) "TIMESTAMP_IA")))))
+		(list (elgantt--normalize-date-string (car dates)) (elgantt--normalize-date-string (cadr dates))))))))
+    ('category
+     (cdar (org-entry-properties (point) "CATEGORY")))
+    ('deadline
+     (when (cdr (car (org-entry-properties (point) "DEADLINE")))
+       (if (string= (cdr (car (org-entry-properties (point) "DEADLINE"))) (elgantt--get-property-from-org-point))
+	   nil
+	 (elgantt--normalize-date-string (cdr (car (org-entry-properties (point) "DEADLINE")))))))
+    ('hashtag
+     (-first (lambda (tagstring) (s-starts-with-p "#" tagstring))
+	     (s-split ":"
+		      (cdar (org-entry-properties (point) "ALLTAGS")))))
+    (_ (user-error "%s is not a valid argument."))))
 
 
 
